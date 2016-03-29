72df3ef4aebc547227f0f7c82fb922e92ffa53dd
MSturges <Sturges3232@gmail.com>
2016-03-29 12:21:16 -0600

    fallowed along

--- migrations/20160329121304_create_humans.js
+++ migrations/20160329121304_create_humans.js
@@ -1,10 +1,8 @@
-
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('humans', function (table) {
     table.increments();
     table.string('name');
     table.integer('age');
-    table.string('photo');
     table.timestamps();
   })
 };
--- routes/index.js
+++ routes/index.js
@@ -4,45 +4,7 @@ var knex = require('knex')(require('../knexfile')['development']);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  knex('humans').then(function (results) {
-    if (results.length !== 0) {
-      res.render('index', { humans: results });
-    }else{
-      res.render('index', { error: 'no humans added' });
-    }
-
-  })
-
-});
-
-router.post('/addhuman', function(req, res, next) {
-  if (req.body.name && req.body.age) {
-    knex('humans').insert(req.body)
-    .then(function(response){
-      res.redirect('/');
-    })
-  } else {
-    res.render('index', { error: 'Please fill out the form' });
-  }
-});
-
-
-router.post('/edithuman/:id', function(req,res,next){
-  knex.table('humans')
-  .where({id: req.params.id})
-  .update(req.body).then(function(response){
-    res.redirect('/');
-  })
-});
-
-
-router.post('/removehuman/:id', function(req, res, next) {
-  knex('humans')
-  .where('id', req.params.id)
-  .del().then(function(response){
-    res.redirect('/');
-  })
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
